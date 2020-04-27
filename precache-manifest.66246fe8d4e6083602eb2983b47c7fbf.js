fd8aa3dd2502d19d6f21b1c4309bbb86e140e809
dburtonthompson <dburtonthompson@gmail.com>
2020-04-27 08:12:31 -0400

    Updates

---
+++ precache-manifest.66246fe8d4e6083602eb2983b47c7fbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ef31f21d565756f0a04d46af4f900a",
+    "url": "/react-app-/index.html"
+  },
+  {
+    "revision": "fdb6aa695aff820b7f83",
+    "url": "/react-app-/static/css/main.52e0ab91.chunk.css"
+  },
+  {
+    "revision": "a51854b58a58d8a97314",
+    "url": "/react-app-/static/js/2.18de4f7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-/static/js/2.18de4f7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdb6aa695aff820b7f83",
+    "url": "/react-app-/static/js/main.073c8a3c.chunk.js"
+  },
+  {
+    "revision": "1bc6935f1807f80e3f4d",
+    "url": "/react-app-/static/js/runtime-main.743eb60e.js"
+  },
+  {
+    "revision": "3accf9d5a45ef0a5fc346fcb441fde78",
+    "url": "/react-app-/static/media/image.3accf9d5.jpeg"
+  }
+]);
